82fc851b14955362380add7bff5647b8e246d785
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-02 05:49:03 +0500

    4-misol

--- bin/basic_math_function04.dart
+++ bin/basic_math_function04.dart
@@ -7,4 +7,13 @@
     Returns:
         result (double): multiply x by the sine of y. Round the result.
 */
-void main() {}
+import 'dart:math';
+
+double func(double y, int x) {
+  double c = sin(y) * x;
+  return c;
+}
+
+void main() {
+  print(func(pi/2,3));
+}
