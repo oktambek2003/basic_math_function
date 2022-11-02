d83dd0813650b7c7245092a94199e638773889f6
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-02 06:13:37 +0500

    6-misol

--- bin/basic_math_function06.dart
+++ bin/basic_math_function06.dart
@@ -8,5 +8,17 @@ Create function named func with a argument
       result (float): Raise cosine of x and the sine of y to the second power
       Round the result before returning the sum of the raised values
 */
+import 'dart:math';
 
-void main() {}
+double func(double y, double x) {
+  double a = cos(pi / 3);
+  double b = sin(pi / 3);
+  double c = a + b;
+  int e = c.round();
+  double g = e.toDouble();
+  return g;
+}
+
+void main() {
+  print(func(pi / 3, pi / 3));
+}
